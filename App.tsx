f951018082e18c376d6912e9045918a77f520a34
frantecbh <franciscomenezesbh@gmail.com>
2022-09-21 18:38:36 -0300

    configurando thema

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 
 import { Grupos } from '@screens/Grupos';
+import { ThemeProvider } from 'styled-components';
+import theme from '@stheme/index';
 
 export default function App() {
   return (
-    <Grupos />
+    <ThemeProvider theme={theme}>
+      <Grupos />
+    </ThemeProvider>
+
   );
 }
 
