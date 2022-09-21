59f6dcbe2d1187a1ed8245a400344944fc01e1d0
frantecbh <franciscomenezesbh@gmail.com>
2022-09-21 18:03:29 -0300

    base aplicação

---
+++ src/screens/Grupos/index.tsx
@@ -0,0 +1,20 @@
+
+import { StyleSheet, Text, View } from 'react-native';
+
+export function Grupos() {
+  return (
+    <View style={styles.container}>
+      <Text>Grupos</Text>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
