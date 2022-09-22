68db529d37b2971f04cc5eaaa8bab93c542a3ae0
frantecbh <franciscomenezesbh@gmail.com>
2022-09-21 22:56:01 -0300

    component Highlight

--- src/screens/Grupos/index.tsx
+++ src/screens/Grupos/index.tsx
@@ -1,12 +1,15 @@
 
 
 import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+
 import { Container,  } from './styles';
 
 export function Grupos() {
   return (
     <Container>
       <Header />
+      <Highlight title='Turmas' subtitle='Jogue com sua turma'/>
     </Container>
 
 
