d9158f2c59dec1d35fe14015d03c8da433d7b7df
frantecbh <franciscomenezesbh@gmail.com>
2022-09-21 23:29:19 -0300

    Lista de grupos

--- src/screens/Grupos/index.tsx
+++ src/screens/Grupos/index.tsx
@@ -1,22 +1,32 @@
-
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 
+
 import { Container,  } from './styles';
 
 export function Grupos() {
 
-  function handleTurma(){
-    console.log("clicou")
-  }
+  const [groups, setGroups] = useState<string[]>(['Galera da Rocket', 'Amigos'])
+ 
 
   return (
     <Container>
       <Header />
       <Highlight title='Turmas' subtitle='Jogue com sua turma'/>
-      <GroupCard title='Galera do Ignite' onPress={handleTurma}/>
+
+    <FlatList 
+    data={groups}
+    keyExtractor={item => item}
+    renderItem={({item}) => (
+        <GroupCard title={item} />
+    )}
+    
+    />
+      
     </Container>
 
 
